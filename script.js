681b49575e6ebc028e432471c6aecdedfe49e95d
flandrea <flande14@gmail.com>
2020-12-10 20:30:36 -0500

    pushing array

---
+++ script.js
@@ -0,0 +1,18 @@
+const sandwiches = [
+    { name: "BLT", description: "Bacon, lettucs, & tomato", price: 5.00},
+    { name: "Caprese", description: "Mozzarella, tomato, & basil", price: 7.50},
+    { name: "PBJ", description: "Peanut butter & jelly time!", price: 3.50},
+    { name: "Turkey Club", description: "Turkey, swiss, lettuce, & tomato", price: 8.50},
+]
+
+const sides = [
+    { name: "Chips", description: "Homemade kettle baked cajun seasoned chips - best in town!", price: 2.50},
+    { name: "Chocolate Chip Cookie", description: "Fresh chocolate chip cookies, as good as your mom makes", price: 2.50},
+]
+
+const drinks = [
+    { name: "Cola", description: "You know the deal", price: 2.50},
+    { name: "Diet Cola", description: "You on a diet?", price: 2.50},
+    { name: "Sprite", description: "Lemon-Lime greatness!", price: 2.50},
+    { name: "Lemonade", description: "Fresh squeezed lemonade", price: 3.25},
+]
