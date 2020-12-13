e6db89ce8c8dcaaf2e7d4423e0ceb73536a916fa
StaceyFolsom <staceyannfolsom@gmail.com>
2020-12-13 00:46:53 -0500

    fixed class names for consistency

--- script.js
+++ script.js
@@ -32,25 +32,25 @@ const submitButton = document.querySelector(".checkout");
 submitButton.addEventListener("click", function (event) {
   console.log(event);
 });
-{
-  const data = new FormData(orderForm);
+// {
+//   const data = new FormData(orderForm);
 
-  const blt = data.get("blt-qty");
-  const caprese = data.get("cap-qty");
-  const pjb = data.get("pbj-qty");
-  const tc = data.get("tc-qty");
-  const chips = data.get("chips-qty");
-  const cookies = data.get("cookie-qty");
-  const coke = data.get("cookie-qty");
-  const diet = data.get("diet-qty");
-  const sprite = data.get("sprite-qty");
-  const lemonade = data.get("lemonade-qty");
-}
-console.log(FormData);
+//   const blt = data.get("blt-qty");
+//   const caprese = data.get("cap-qty");
+//   const pbj = data.get("pbj-qty");
+//   const tc = data.get("tc-qty");
+//   const chips = data.get("chips-qty");
+//   const cookies = data.get("cookie-qty");
+//   const cola = data.get("cola-qty");
+//   const diet = data.get("diet-qty");
+//   const sprite = data.get("sprite-qty");
+//   const lemonade = data.get("lemonade-qty");
+// }
+// console.log(FormData);
 
-const subtotal = (blt*5)+(caorese*7.5)+(pbj*3.5)+(tc*8.5)+(chips*2.5)+(cookies*2.5)+(coke*2.5)+(diet*3.5)+(sprite*2)+(lemondae*2.25);
-const tax= subtotal*0.06;
-const total= subtotal+tax;
+// const subtotal = (blt*5)+(caprese*7.5)+(pbj*3.5)+(tc*8.5)+(chips*2.5)+(cookies*2.5)+(cola*2.5)+(diet*2.5)+(sprite*2.5)+(lemondae*3.25);
+// const tax= subtotal*0.06;
+// const total= subtotal+tax;
 
 // //looping and adding items
 // for(const i=0; i < numberOfItems; i++){
@@ -71,18 +71,6 @@ const total= subtotal+tax;
 //     return subtotal;
 // }
 // const elements = document.querySelectorAll('.sandwich-qty');
-
-// Event listener for click on Add button
-// const addButton = document.querySelector(".add");
-// addButton.addEventListener("click", function(event) {
-//     console.log(event);
-// });
-
-// // Event listener for click on Submit button
-// const submitButton = document.querySelector(".checkout");
-// submitButton.addEventListener("click", function(event) {
-//     console.log(event);
-// });
 
 // Displays number of items in cart
 const itemCount = document.querySelector("#itm-count");
